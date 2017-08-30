87c46b5625bf936b891e9e3f3d9d77ecfad98da6
wkigen <qiji950@163.com>
2017-08-30 11:23:21 +0800

    base

--- baselib/src/main/java/com/vicky/android/baselib/refreshlayout/RefreshHolderUtil.java
+++ baselib/src/main/java/com/vicky/android/baselib/refreshlayout/RefreshHolderUtil.java
@@ -7,7 +7,7 @@ import com.vicky.android.baselib.R;
 public class RefreshHolderUtil {
     public static RefreshViewHolder getHolder(Context ctx) {
         MeiTuanRefreshViewHolder meiTuanRefreshViewHolder = new MeiTuanRefreshViewHolder(ctx, true);
-        meiTuanRefreshViewHolder.setPullDownImageResource(R.mipmap.yd_refresh_loading01);
+        meiTuanRefreshViewHolder.setPullDownImageResource(R.mipmap.refresh_loading01);
         meiTuanRefreshViewHolder.setChangeToReleaseRefreshAnimResId(R.drawable.bga_refresh_mt_change_to_release_refresh);
         meiTuanRefreshViewHolder.setRefreshingAnimResId(R.drawable.bga_refresh_mt_refreshing);
         return meiTuanRefreshViewHolder;
@@ -15,7 +15,7 @@ public class RefreshHolderUtil {
 
     public static RefreshViewHolder getHolder(Context ctx, boolean loadMoreEnable) {
         MeiTuanRefreshViewHolder meiTuanRefreshViewHolder = new MeiTuanRefreshViewHolder(ctx, loadMoreEnable);
-        meiTuanRefreshViewHolder.setPullDownImageResource(R.mipmap.yd_refresh_loading01);
+        meiTuanRefreshViewHolder.setPullDownImageResource(R.mipmap.refresh_loading01);
         meiTuanRefreshViewHolder.setChangeToReleaseRefreshAnimResId(R.drawable.bga_refresh_mt_change_to_release_refresh);
         meiTuanRefreshViewHolder.setRefreshingAnimResId(R.drawable.bga_refresh_mt_refreshing);
         return meiTuanRefreshViewHolder;
